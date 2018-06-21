ed810ec66325b2531b26e3f5ac85dac058e8ea1e
Goekhan Oezcan <goekhan_oezcan@mentor.com>
2018-06-21 15:26:38 +0200

    adv728x: add initial driver 3.0.0
    Signed-off-by: Goekhan Oezcan <goekhan_oezcan@mentor.com>

---
+++ drivers/media/platform/intel/intel-ipu4-apl-polaris-pdata.c
@@ -0,0 +1,73 @@
+/*
+ * Copyright (c) 2015--2016 Intel Corporation.
+ * Copyright (c) 2018 Mentor - a Siemens Business 
+ * 
+ * Based on intel-ipu4-bxt-p-pdata.c
+ *
+ * Author: Samu Onkalo <samu.onkalo@intel.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License version
+ * 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+#include <linux/clk.h>
+#include <linux/clkdev.h>
+#include <linux/gpio.h>
+#include <linux/kernel.h>
+#include <linux/pci.h>
+
+#include <media/intel-ipu4-isys.h>
+
+#define GPIO_BASE             434
+
+#if defined(CONFIG_VIDEO_ADV728X) || defined(CONFIG_VIDEO_ADV728X_MODULE)
+static struct intel_ipu4_isys_csi2_config adv7282_csi2_cfg = {
+	.nlanes = 1,
+	.port = 5,
+};
+
+static struct intel_ipu4_isys_subdev_info adv7282_sd = {
+	.csi2 = &adv7282_csi2_cfg,
+	.i2c = {
+		.board_info = {
+			 .type = "adv728x",
+			 .flags = 0,
+			 .addr = 0x20,
+			 .platform_data = NULL,
+			 .irq = GPIO_BASE + 22, /* GPIO_22 */
+		},
+		.i2c_adapter_id = 0,
+	}
+};
+#endif
+
+/*
+ * Map buttress output sensor clocks to sensors -
+ */
+static struct intel_ipu4_isys_clk_mapping clk_mapping[] = {
+	{ CLKDEV_INIT(NULL, NULL, NULL), NULL }
+};
+
+static struct intel_ipu4_isys_subdev_pdata pdata = {
+	.subdevs = (struct intel_ipu4_isys_subdev_info *[]) {
+#if defined(CONFIG_VIDEO_ADV728X) || defined(CONFIG_VIDEO_ADV728X_MODULE)
+		&adv7282_sd,
+#endif
+		NULL,
+	},
+	.clk_map = clk_mapping,
+};
+
+static void ipu4_quirk(struct pci_dev *pci_dev)
+{
+	pci_dev->dev.platform_data = &pdata;
+}
+
+DECLARE_PCI_FIXUP_EARLY(PCI_VENDOR_ID_INTEL, 0x5a88,
+			ipu4_quirk);
