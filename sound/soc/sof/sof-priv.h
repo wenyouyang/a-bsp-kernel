thesofproject/linux

ASoC: SOF: make sof_complete_pipeline non static
This will be called during resume to send ipc for pipeline
completion.

Signed-off-by: Ranjani Sridharan <ranjani.sridharan@linux.intel.com>

sound/soc/sof/sof-priv.h
@@ -456,6 +456,8 @@ int snd_sof_init_topology(struct snd_sof_dev *sdev,
 			  struct snd_soc_tplg_ops *ops);
 int snd_sof_load_topology(struct snd_sof_dev *sdev, const char *file);
 void snd_sof_free_topology(struct snd_sof_dev *sdev);
+int sof_complete_pipeline(struct snd_sof_dev *sdev,
+			  struct snd_sof_widget *swidget);
 
 /*
  * Trace/debug

sound/soc/sof/topology.c
@@ -1961,10 +1961,9 @@ static int sof_route_unload(struct snd_soc_component *scomp,
 	return 0;
 }
 
-static int sof_complete_pipeline(struct snd_soc_component *scomp,
-				 struct snd_sof_widget *swidget)
+int sof_complete_pipeline(struct snd_sof_dev *sdev,
+			  struct snd_sof_widget *swidget)
 {
-	struct snd_sof_dev *sdev = snd_soc_component_get_drvdata(scomp);
 	struct sof_ipc_pipe_ready ready;
 	struct sof_ipc_reply reply;
 	int ret;
@@ -1999,7 +1998,7 @@ static void sof_complete(struct snd_soc_component *scomp)
 		switch (swidget->id) {
 		case snd_soc_dapm_scheduler:
 			swidget->complete =
-				sof_complete_pipeline(scomp, swidget);
+				sof_complete_pipeline(sdev, swidget);
 			break;
 		default:
 			break;

wenyouyang/a-bsp-kernel

sound/soc/sof/sof-priv.h
@@ -456,6 +456,8 @@ int snd_sof_init_topology(struct snd_sof_dev *sdev,
 			  struct snd_soc_tplg_ops *ops);
 int snd_sof_load_topology(struct snd_sof_dev *sdev, const char *file);
 void snd_sof_free_topology(struct snd_sof_dev *sdev);
+int snd_sof_complete_pipeline(struct snd_sof_dev *sdev,
+			      struct snd_sof_widget *swidget);
 
 /*
  * Trace/debug

sound/soc/sof/topology.c
@@ -1998,10 +1998,9 @@ static int sof_route_unload(struct snd_soc_component *scomp,
 	return 0;
 }
 
-static int sof_complete_pipeline(struct snd_soc_component *scomp,
-				 struct snd_sof_widget *swidget)
+int snd_sof_complete_pipeline(struct snd_sof_dev *sdev,
+			      struct snd_sof_widget *swidget)
 {
-	struct snd_sof_dev *sdev = snd_soc_component_get_drvdata(scomp);
 	struct sof_ipc_pipe_ready ready;
 	struct sof_ipc_reply reply;
 	int ret;
@@ -2036,7 +2035,7 @@ static void sof_complete(struct snd_soc_component *scomp)
 		switch (swidget->id) {
 		case snd_soc_dapm_scheduler:
 			swidget->complete =
-				sof_complete_pipeline(scomp, swidget);
+				snd_sof_complete_pipeline(sdev, swidget);
 			break;
 		default:
 			break;